d1719561d1676df8e9969ff778f505f79a3486f9
aap9002 <aap9002@gmail.com>
2022-12-12 11:51:53 +0000

    change to state under construction

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <b>Alan Prophett</b> - Portfolio under construction
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/AAP9002"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Github
         </a>
       </header>
     </div>
